55cd596beff9e2604a155332acf5b7da684c168d
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-18 17:39:13 +0900

    feat: 開発用のトップページを作成

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import Router from 'express-promise-router';
+import path from 'path';
 import * as apiController from './apiContoroller';
 
 const router = Router();
 
-router.get('/', (req, res) => res.json('Hello World'));
 router.get('/api/entries', apiController.showAllEntries);
+router.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '..','public', 'index.html')));
 
 export default router;
