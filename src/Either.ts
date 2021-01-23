92cbec019dc48e1fd7658cf85169fcb3e773fb37
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-23 13:58:31 +0900

    refactor: Eitherの型パズル.this型、genericsをつけてnewする

--- src/Either.ts
+++ src/Either.ts
@@ -1,40 +1,38 @@
 type f<T, U> = (x: T) => U;
-type TEither<L, R> = Either<L, undefined> | Either<undefined, R>;
+type _ = undefined;
 
-class Either<L = undefined, R = undefined> {
+class Either<L, R> {
   constructor(
     private readonly lr: Left<L>|Right<R>
   ) {}
-  public map = <T>(fn: f<R, T>): TEither<L, T> => {
+  public map = <T>(fn: f<R, T>): this | Either<undefined, T> => { // 返り値をthis型でごまかす
     return this.lr instanceof Left
-      ? ofLeft(this.lr.value)
+      ? this
       : ofRight(fn(this.lr.value));
   }
   public isLeft = () => this.lr instanceof Left;
   public isRight = () => this.lr instanceof Right;
 }
 
-const ofRight = <T>(val: T) => new Either(new Right(val));
-const ofLeft = <T>(val: T) => new Either(new Left(val));
-const fromNullable = <T>(val: T) => val === null || val === undefined
+const ofRight = <T>(val: T) => new Either<undefined, T>(new Right(val)); // genericsをつけてnewする
+const ofLeft = <T>(val: T) => new Either<T, undefined>(new Left(val));
+const fromNullable = <T>(val: T) => (val === null || val === undefined)
   ? ofLeft(val)
   : ofRight(val);
 
 class Left<T> {
-  public readonly status = 'Left';
   constructor(
     public readonly value: T,
   ){}
 }
 
 class Right<T> {
-  public readonly status = 'Right';
   constructor(
     public readonly value: T,
   ){}
 }
 
-ofRight(123)
+ofRight(1234)
   .map((x: number) => x * 2)
   .map((x: number) => console.log(x))
   ;
