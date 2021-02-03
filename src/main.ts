7b1caec97a42f404a8aedab9e0827ffe7d724e04
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-03 14:50:39 +0900

    feat: corsを許可

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ const port = process.env.PORT || 3000;
 
 const app = express()
 //  .use(helmet())
+  // allow cors
+  .use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  })
   // analyze request body
   .use(express.urlencoded({ extended: false }))
   .use(express.json())
