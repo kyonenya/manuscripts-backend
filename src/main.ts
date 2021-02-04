ec8276e37d87fb7997de9e5bbc517490ba42fb41
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-04 22:39:21 +0900

    feat: corsを許可 POST, PUTなど個別のメソッドに対しても

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,20 @@ const port = process.env.PORT || 3000;
 
 const app = express()
 //  .use(helmet())
-  // allow cors
+  /**
+   * allow cors
+   * @url https://s8a.jp/node-js-express-http-options
+   */
   .use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Max-Age', '86400');
     next();
+  })
+  .options('*', (req, res) => {
+    res.sendStatus(200);
   })
   // analyze request body
   .use(express.urlencoded({ extended: false }))
