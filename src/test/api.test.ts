c5dee54595d6a10bb313993b929a577be618065e
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-28 12:49:27 +0900

    feat: Updateのタグのテスト追加 バグがあるので直す

--- src/test/api.test.ts
+++ src/test/api.test.ts
@@ -59,6 +59,7 @@ describe('Api', () => {
       .then(response => response.json())
       .catch(err => console.error(err));
     assert.strictEqual(result.uuid, uuid);
+    assert.deepStrictEqual(result.tags, entry.tags);
     assert.strictEqual(result.text, entry2.text);
     assert.strictEqual(result.starred, entry2.starred);
   });
