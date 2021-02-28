1c9eae2a528d22ca2d361d1f78d532e9593f8cf2
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-28 17:28:30 +0900

    feat: PromiseでラップされたEitherのテストケース

--- src/Either.ts
+++ src/Either.ts
@@ -11,9 +11,16 @@ export class Either<L, R> {
       ? Either.ofLeft(this._obj.value)
       : Either.ofRight(fn(this._obj.value));
   }
+//  public awaitMap = <T>(fn: f<R, T>): Either<L, T> => {
+//    const resolved = this._obj.then(x => x);
+//  }
   public bind = <T>(fn: f<R, T>): L|T => {
     return this._obj.status === 'Left'
       ? this._obj.value
       : fn(this._obj.value);
   }
+}
+
+export class EitherP<L, R> {
+  
 }
--- src/test/Either.test.ts
+++ src/test/Either.test.ts
@@ -22,4 +22,28 @@ describe('Either', () => {
       .map(x => assert.strictEqual(x, 'valid?id=123'));
       ;
   });
+  it('map then', () => {
+    const lazyDouble = (x: number): Promise<number> => new Promise((resolve, reject) => resolve(x * 2));
+    Either.ofRight(123)
+      .map(lazyDouble)
+      .map(x => x.then(x => x))
+      .map(x => x.then(x => assert.strictEqual(x, 246)))
+      ;
+  });
+  it('bind then', () => {
+    const lazyDecode = async (url: string): Promise<Either<string, string>> => {
+      const promisedDecode = (url: string): Promise<string> => new Promise((resolve, reject) => resolve(decodeURIComponent(url)));
+      try {
+        const result = await promisedDecode(url);
+        return Either.ofRight(result);
+      } catch (err) {
+        return Either.ofLeft(`${err}`);
+      }
+    };
+    Either.ofRight('valid%3Fid%3D')
+      .map(lazyDecode)
+      .map(x => x.then(x => x))
+      .map(x => x.then(x => assert.strictEqual(x, 'valid?id=123')))
+      ;
+  });
 });
