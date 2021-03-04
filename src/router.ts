c018f0f26fec4b6a0f427d73987e94cdbcdd65f5
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-03-04 13:12:25 +0900

    fix: testContorollerを削除

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import Router from 'express-promise-router';
 import path from 'path';
 import * as apiController from './apiController';
-import * as testController from './testController';
 
 export const router = Router()
   .get('/api/entries', apiController.readAllEntries)
@@ -9,9 +8,5 @@ export const router = Router()
   .post('/api/entries/:uuid', apiController.createNewEntry)
   .put('/api/entries/:uuid', apiController.updateEntry)
   .delete('/api/entries/:uuid', apiController.deleteEntry)
-  .get('/test/entries/create', testController.testCreate)
-  .get('/test/entries/read/:uuid', testController.testRead)
-  .get('/test/entries/update/:uuid', testController.testUpdate)
-  .get('/test/entries/delete/:uuid', testController.testDelete)
   .get('/', (req, res) => res.sendFile(path.resolve(__dirname, '..','public', 'index.html')))
   ;
