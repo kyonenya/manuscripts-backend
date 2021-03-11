b8dac3ce79ff0f6b21abcf953170f88b45738a9e
kyonenya <kyon.musica@gmail.com>
2021-03-11 14:11:31 +0000

    fix: firebase SDKの初期化 private keyのみ環境変数に

--- src/firebaseAdmin.ts
+++ src/firebaseAdmin.ts
@@ -6,7 +6,7 @@ dotenv.config();
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: 'manuscripts-kyonenya',
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientEmail: 'firebase-adminsdk-36vwk@manuscripts-kyonenya.iam.gserviceaccount.com',
     privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n')
   }),
 });
