0a37d71868b74776c98b31de392033f4cd513fbe
kyonenya <kyon.musica@gmail.com>
2021-03-11 13:48:50 +0000

    feat: idトークン付きAPIのテスト（＋カスタムトークン→idトークンへの変換関数）

--- src/test/api.test.ts
+++ src/test/api.test.ts
@@ -1,8 +1,9 @@
 import assert from 'assert';
 import fetch from 'node-fetch';
+import admin, { uid } from '../firebaseAdmin';
 
-describe('Api', () => {
-  const uuid = '8cb4f18cccdf4422b54010fd96711ee9';
+describe('Api', async () => {
+  const uuid = '8cb4f18cccdf4422b54010fd96711ee7';
   const baseUrl = 'http://localhost:3000';
 //  const baseUrl = 'https://manuscripts.herokuapp.com';
 
@@ -33,10 +34,14 @@ describe('Api', () => {
     assert.deepStrictEqual(result.tags, entry.tags);
   });
   it('ReadAll limit=3', async () => {
+    const idToken = await getIdToken(uid);
     const limit = 3;
     const result = await fetch(`${baseUrl}/api/entries?limit=${limit}`, {
       method: 'GET',
-      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        'Authorization': `Bearer: ${idToken}`,
+      },
     })
       .then(response => response.json())
       .catch(err => console.error(err));
@@ -72,3 +77,21 @@ describe('Api', () => {
     assert.strictEqual(result, uuid);
   });
 });
+
+const getIdToken = async (uid: string): Promise<string> => {
+  const apiKey = 'AIzaSyAttARzXFbAreQhdIaAKPMsn6bPzbTMA8o';
+  const customToken = await admin.auth().createCustomToken(uid);
+  const res = await fetch(`https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=${apiKey}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        token: customToken,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then(response => response.json())
+      .catch(err => console.error(err));
+  return res.idToken;
+};
