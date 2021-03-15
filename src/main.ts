83a56064e128056d7254140a1cb4912c2ac6cfb1
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-03-15 22:31:56 +0900

    fix: cors設定をミドルウェアに任せる

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import express from 'express';
-import admin from 'firebase-admin';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { router } from './router';
-import { allowCors } from './allowCors';
 
 dotenv.config();
-
 const port = process.env.PORT || 3000;
 
 const app = express()
-  .use(allowCors)
+  .use(cors())
   .options('*', (req, res) => res.sendStatus(200)) // preflight
   .use(express.urlencoded({ extended: false })) // analyze request body
   .use(express.json())
