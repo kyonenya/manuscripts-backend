f6c29ef3e38b74a81c30a8330be90e1fbbe10611
kyonenya <kyon.musica@gmail.com>
2021-03-18 09:17:12 +0000

    fix: 500エラー時にエラーレスポンスを返す 暫定的に

--- src/errorController.ts
+++ src/errorController.ts
@@ -1,12 +1,12 @@
 import { RequestHandler, ErrorRequestHandler } from 'express';
 
 export const notFound: RequestHandler = (req, res) => {
-  res.status(404);
+  res.sendStatus(404);
   res.send('404 | ルートが見つかりません');
 };
 
-export const internalError: ErrorRequestHandler = (err, req, res, next) =>{
+export const internalError: ErrorRequestHandler = (err, req, res) =>{
   console.error(err.stack);
-  res.status(500);
-  res.send(`500 | Sorry, our application is experiencing a problem.`);
+  res.sendStatus(500);
+  res.send(err.toString());
 };
