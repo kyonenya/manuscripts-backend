4468e7889f2b2cf8ec4970faa2880cf621638706
kyonenya <kyon.musica@gmail.com>
2021-03-18 09:21:16 +0000

    fix: res.sendStatus -> res.status

--- src/errorController.ts
+++ src/errorController.ts
@@ -1,12 +1,12 @@
 import { RequestHandler, ErrorRequestHandler } from 'express';
 
 export const notFound: RequestHandler = (req, res) => {
-  res.sendStatus(404);
+  res.status(404);
   res.send('404 | ルートが見つかりません');
 };
 
 export const internalError: ErrorRequestHandler = (err, req, res) =>{
   console.error(err.stack);
-  res.sendStatus(500);
+  res.status(500);
   res.send(err.toString());
 };
