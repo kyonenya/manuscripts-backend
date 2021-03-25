edb95f07c4bf9fbb9cd06482f28cbec9260dd01e
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-03-25 12:49:05 +0900

    fix: エラーをBoomで投げる

--- src/apiRequest.ts
+++ src/apiRequest.ts
@@ -16,34 +16,34 @@ export const entitize = (req: Request) =>
 
 export const validateToken = async (req: Request) => {
   if (!req.headers['authorization']) {
-    throw Boom.unauthorized('認証エラー: idトークンが空です');
+    throw Boom.unauthorized('idトークンが空です');
   }
   const idToken = req.headers['authorization'].split(' ')[1];
   const decoded = await authApp.verifyIdToken(idToken);
   if (decoded.uid !== uid) {
-    throw Boom.unauthorized('認証エラー: 異なるuidです');
+    throw Boom.unauthorized('異なるuidです');
   }
   return req;
 };
 
 export const validateToken2 = (req: Request): TE.TaskEither<any, any> => async () => {
   if (!req.headers['authorization']) {
-    return E.left(Boom.unauthorized('認証エラー: idトークンが空です'));
+    return E.left(Boom.unauthorized('idトークンが空です'));
   }
   const idToken = req.headers['authorization'].split(' ')[1];
   const decoded = await authApp.verifyIdToken(idToken);
   if (decoded.uid !== uid) {
-    E.left(Boom.unauthorized('認証エラー: 異なるuidです'));
+    E.left(Boom.unauthorized('異なるuidです'));
   }
   return E.right(req);
 };
 
 export const limitQuery = (req: Request): number => {
-  if (!req.query.limit) throw new Error('件数が指定されていません');
+  if (!req.query.limit) throw Boom.badRequest('件数が指定されていません');
   return parseInt(req.query.limit.toString());
 };
 
 export const uuidParams = (req: Request): string => {
-  if (!req.params.uuid) throw new Error('uuidが指定されていません');
+  if (!req.params.uuid) throw Boom.badRequest('uuidが指定されていません');
   return req.params.uuid.toString();
 };
