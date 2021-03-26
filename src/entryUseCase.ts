86999e356044cf1a77b10a5c4803bc425a4927a9
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-03-27 06:28:07 +0900

    feat: createAllのUseCase taskSeqで1件づつ順次実行

--- src/entryUseCase.ts
+++ src/entryUseCase.ts
@@ -1,17 +1,24 @@
 import * as TE from 'fp-ts/lib/TaskEither'
+import * as A from 'fp-ts/lib/Array'
+import { sequenceT } from 'fp-ts/lib/Apply';
 import { identity } from 'fp-ts/lib/function';
+import Boom from '@hapi/boom';
 import { PoolClient } from 'pg';
 import * as entriesRepository from './entriesRepository';
 import * as tagsRepository from './tagsRepository';
 import { Entry } from './entryEntity';
 
-export const createOne = (client: PoolClient) => (entry: Entry) : TE.TaskEither<any, any> => {
+export const createOne = (client: PoolClient) => (entry: Entry) : TE.TaskEither<unknown, Entry> => {
   const entriesInvoker = entriesRepository.insertOne(client);
   const tagsInvoker = tagsRepository.insertAll(client);
   return TE.tryCatch(
     () => Promise.all([entriesInvoker(entry), tagsInvoker(entry.tags, entry.uuid)]).then(_ => entry),
     identity
   )
+};
+
+export const createAll = (client: PoolClient) => (entries: Entry[]) => {
+  return A.array.sequence(TE.taskEitherSeq)(entries.map(entry => createOne(client)(entry)));
 };
 
 export const readAll = (client: PoolClient) => (limit: number): Promise<Entry[]> => {
