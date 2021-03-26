97b32483cfbfcfa333abbeadb344dac2abf5bba1
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-03-27 06:30:35 +0900

    fix: ファイルパスの相対パス

--- src/test/jsonRepository.test.ts
+++ src/test/jsonRepository.test.ts
@@ -4,7 +4,7 @@ import * as jsonRepository from '../jsonRepository';
 
 describe('jsonRepository', () => {
   it('readAll', async () => {
-    const filePath = path.resolve(__dirname, '..', 'assets', 'dayone-210320.json');
+    const filePath = path.resolve(__dirname, '..', '..', 'assets', 'dayone-210320.json');
     const entries = await jsonRepository.readAll({ filePath });
     console.log(entries);
   });
