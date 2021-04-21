5f7e6aaad99324eb0fd68246574df9ab1fd2f2c3
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-04-21 12:00:30 +0900

    delete: デッドコード

--- src/errorController.ts
+++ src/errorController.ts
@@ -5,19 +5,3 @@ import { Boom } from '@hapi/boom';
 export const notFound: RequestHandler = (req, res) => {
   res.status(404).send('404 | ルートが見つかりません');
 };
-
-//export const internalError: ErrorRequestHandler = (err, req, res, next) =>{
-//  console.error(err);
-//  if (err instanceof Boom) {
-//    return res.status(err.output.statusCode).json({
-//      error: err.output.payload
-//    });
-//  }
-//  return res.status(err.statusCode).json({
-//    error: {
-//      statusCode: err.statusCode,
-//      name: err.name,
-//      message: err.message,
-//    },
-//  });
-//};
