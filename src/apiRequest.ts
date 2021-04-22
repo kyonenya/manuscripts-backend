d68332b55d7942fb9a44482abdf9df57c98c55ee
kyonenya <kyon.musica@gmail.com>
2021-04-22 09:07:22 +0000

    refactor: Boomを個別インポート エラー番号を削除

--- src/apiRequest.ts
+++ src/apiRequest.ts
@@ -1,5 +1,5 @@
 import { Request } from 'express';
-import Boom from '@hapi/boom';
+import { Boom, badRequest, unauthorized } from '@hapi/boom';
 import * as AP from 'fp-ts/lib/Apply';
 import * as E from 'fp-ts/lib/Either';
 import * as TE from 'fp-ts/lib/TaskEither';
@@ -19,54 +19,54 @@ export const entitize = (req: Request): Entry =>
 
 export const validateToken = (
   req: Request
-): TE.TaskEither<Boom.Boom<401>, Request> => async () => {
+): TE.TaskEither<Boom, Request> => async () => {
   if (!req.headers['authorization']) {
-    return E.left(Boom.unauthorized('IDトークンを送信してください'));
+    return E.left(unauthorized('IDトークンを送信してください'));
   }
   const idToken = req.headers['authorization'].split(' ')[1];
   const decoded = await authApp.verifyIdToken(idToken);
   if (decoded.uid !== uid) {
-    E.left(Boom.unauthorized('正しいユーザーのIDトークンを送信してください'));
+    E.left(unauthorized('正しいユーザーのIDトークンを送信してください'));
   }
   return E.right(req);
 };
 
 export const limitQuery = (req: Request): number => {
   if (!req.query.limit)
-    throw Boom.badRequest('取得したい記事の件数を指定してください');
+    throw badRequest('取得したい記事の件数を指定してください');
   return parseInt(req.query.limit.toString());
 };
 
-const limitQuery2 = (req: Request): E.Either<Boom.Boom<400>, number> => {
+const limitQuery2 = (req: Request): E.Either<Boom<400>, number> => {
   if (!req.query.limit) return E.right(defaultLimit);
   const limit = Number.parseInt(req.query.limit.toString(), 10);
   return Number.isNaN(limit)
-    ? E.left(Boom.badRequest('記事の件数には整数を指定してください'))
+    ? E.left(badRequest('記事の件数には整数を指定してください'))
     : E.right(limit);
 };
 
-const keywordQuery = (req: Request): E.Either<Boom.Boom<400>, string> => {
+const keywordQuery = (req: Request): E.Either<Boom<400>, string> => {
   if (!req.query.keyword) {
-    return E.left(Boom.badRequest('検索語句を指定してください'));
+    return E.left(badRequest('検索語句を指定してください'));
   }
   return E.right(req.query.keyword.toString());
 };
 
 export const uuidParams = (req: Request): string => {
-  if (!req.params.uuid) throw Boom.badRequest('記事のuuidを指定してください');
+  if (!req.params.uuid) throw badRequest('記事のuuidを指定してください');
   return req.params.uuid.toString();
 };
 
-export const uuidParam2 = (req: Request): E.Either<Boom.Boom<any>, string> => {
+export const uuidParam2 = (req: Request): E.Either<Boom<any>, string> => {
   if (!req.params.uuid) {
-    return E.left(Boom.badRequest('記事のuuidを指定してください'));
+    return E.left(badRequest('記事のuuidを指定してください'));
   }
   return E.right(req.params.uuid.toString());
 };
 
-const tagParam = (req: Request): E.Either<Boom.Boom<400>, string> => {
+const tagParam = (req: Request): E.Either<Boom<400>, string> => {
   if (!req.params.tag) {
-    return E.left(Boom.badRequest('記事のタグを指定してください'));
+    return E.left(badRequest('記事のタグを指定してください'));
   }
   return E.right(req.params.tag.toString());
 };
@@ -74,7 +74,7 @@ const tagParam = (req: Request): E.Either<Boom.Boom<400>, string> => {
 export const readByTagRequest = (
   req: Request
 ): E.Either<
-  Boom.Boom<400>,
+  Boom<400>,
   {
     tag: string;
     limit: number;
@@ -89,7 +89,7 @@ export const readByTagRequest = (
 export const searchKeywordRequest = (
   req: Request
 ): E.Either<
-  Boom.Boom<400>,
+  Boom<400>,
   {
     keyword: string;
     limit: number;
